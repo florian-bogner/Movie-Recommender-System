e1f42a1fd1a001aedaac9173239acef2d2494978
huschpuscheli <bogner.f@googlemail.com>
2022-07-15 13:49:40 +0200

    correct typo

--- Movie_recommender.py
+++ Movie_recommender.py
@@ -86,6 +86,6 @@ elif add_radio == "Item-based":
     item_movies(title_, number)
 elif add_radio == "User-based":
     col_one_list = ratings['userId'].tolist()
-    user_id = st.selectbox('Select a userID:', col_one_list)
+    user_id = st.text_input('Enter a userID in this format :')
     number = st.slider('Select the number of movies you want to be displayed:', 1, 100)
     user_recom(user_id, number)
